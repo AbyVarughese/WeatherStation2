5ce26ab6c9bda73e18ab949da59c318c74f98892
Aby Varughese <97601494+AbyVarughese@users.noreply.github.com>
2022-07-22 12:29:06 +0100

    Add files via upload

---
+++ controllers/dashboard.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const accounts = require("./accounts.js");
+const logger = require("../utils/logger");
+const stationStore = require("../models/station-store");
+const uuid = require("uuid");
+
+const dashboard = {
+  index(request, response) {
+    logger.info("dashboard rendering");
+    const loggedInUser = accounts.getCurrentUser(request);
+    const viewData = {
+      title: "Weather top Dashboard",
+      stations: stationStore.getUserStations(loggedInUser.id)
+    };
+    logger.info("about to render", stationStore.getAllStations());
+    response.render("dashboard", viewData);
+  },
+  async addreport(request, response) {
+    logger.info("rendering new report");
+    let report = {};
+    const lat = request.body.lat;
+    const lng = request.body.lng;
+    const requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&units=metric&appid=YOUR_API_KEY_HERE`
+    const result = await axios.get(requestUrl);
+    if (result.status == 200) {
+      const reading = result.data.current;
+      report.code = reading.weather[0].id;
+      report.temperature = reading.temp;
+      report.windSpeed = reading.wind_speed;
+      report.pressure = reading.pressure;
+      report.windDirection = reading.wind_deg;
+    }
+    console.log(report);
+    const viewData = {
+      title: "Weather Report",
+      reading: report
+    };
+    response.render("dashboard", viewData);
+  },
+  deleteStation(request, response) {
+    const stationId = request.params.id;
+    logger.debug(`Deleting Station ${stationId}`);
+    stationStore.removeStation(stationId);
+    response.redirect("/dashboard");
+  },
+
+  addStation(request, response) {
+    const loggedInUser = accounts.getCurrentUser(request);
+    const newStation = {
+      id: uuid.v1(),
+      userid: loggedInUser.id,
+      title: request.body.title,
+      readings: []
+    };
+    logger.debug("Creating a new Station", newStation);
+    stationStore.addStation(newStation);
+    response.redirect("/dashboard");
+  }
+};
+
+module.exports = dashboard;
